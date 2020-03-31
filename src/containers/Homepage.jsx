b8081df32abc54ee7ddfed701f876dae380a33dc
Alwin van Driel <avandriel1998@gmail.com>
2020-03-31 13:59:00 +0200

    Init

---
+++ src/containers/Homepage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="homepage">
+        <h1>Bountinator</h1>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
