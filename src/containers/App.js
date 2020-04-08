b62191190ff32138680ea0887724a8052128986c
ConanSpithoven <conan.spithoven@gmail.com>
2020-04-08 09:04:42 +0200

    added social share buttons

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Homepage from './Homepage';
+import { Twitter, Facebook, Pinterest  } from 'react-social-sharing';
 
 function App() {
   return (
     <div className="App">
-      <Homepage></Homepage>
+          <Homepage></Homepage>
+          <Facebook solidcircle big link="https://github.com/AvanDriel/Bountinator" />
+          <Twitter solidcircle big message="Check out your own bounty at" link="https://github.com/AvanDriel/Bountinator" />
+          <Pinterest solidcircle big message="Check out your own bounty at" link="https://github.com/AvanDriel/Bountinator" />
     </div>
   );
 }
